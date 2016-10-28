4c7f9f58916d5aa651ce4b7dbf55829689396b94
nostromo <nostromo_@hotmail.it>
2016-10-28 13:43:09 +0200

    add sql lite

--- Corso.Bologna/Corso.Bologna/Services/LocalDataSqlRecipeService.cs
+++ Corso.Bologna/Corso.Bologna/Services/LocalDataSqlRecipeService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Corso.Bologna.Models;
 using PCLStorage;
+using SQLite;
 
 namespace Corso.Bologna.Services
 {
@@ -13,12 +15,25 @@ namespace Corso.Bologna.Services
         public LocalDataSqlRecipeService()
         {
             var rootFolder = FileSystem.Current.LocalStorage;
-           
+          
+            using (SQLiteConnection connection = new SQLiteConnection(Path.Combine(rootFolder.Path, "recipe.db")))
+            {
+                connection.CreateTable<Recipe>();
+                connection.Insert(new Recipe {id = 3, Name = "pippo"});
+                connection.Commit();
+            }
         }
 
-        public Task<IList<Recipe>> GetRecipeAsync()
+        public async Task<IList<Recipe>> GetRecipeAsync()
         {
-            return null;
+            var rootFolder = FileSystem.Current.LocalStorage;
+            List<Recipe> recipes;
+            recipes = new List<Recipe>();
+            using (SQLiteConnection connection = new SQLiteConnection(Path.Combine(rootFolder.Path, "recipe.db")))
+            {
+               recipes.Add(connection.Find<Recipe>(0));
+            }
+            return recipes;
         }
     }
 }
